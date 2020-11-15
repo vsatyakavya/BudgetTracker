88d7f4312d2fc9b7274f042338f5ca4e84b47be6
satya kavya <satyakavya24@gmail.com>
2020-11-15 10:59:08 -0600

    edit file links in service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,8 +5,7 @@ const FILES_TO_CACHE = [
   "/index.html",
 "/manifest.webmanifest",
   "/styles.css",
-  "/db.js",
-  "/index.js",
+ "/index.js",
  "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 

2d1ac2329cfcbe9a0b9bf45f1aee1ce289b40c05
satya kavya <satyakavya24@gmail.com>
2020-11-17 14:48:53 -0600

    edit service worker

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+
 mongoose.connect(
    process.env.MONGODB_URI || 'mongodb://localhost/budget',
 
